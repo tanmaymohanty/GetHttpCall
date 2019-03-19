dddebdbccef453484abfbec01aef2c736ba97418
tanmaymohanty <stmohanty1@gmail.com>
2019-03-19 23:42:14 +0530

    CommitGetCAll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'httpGetService';
+  posts: any;
+  constructor(private http: HttpClient) {
+    http.get('http://jsonplaceholder.typicode.com/users').subscribe(response => {
+      this.posts = response;
+     });
+  }
+
+  ngOnInit() {
+  }
 }
